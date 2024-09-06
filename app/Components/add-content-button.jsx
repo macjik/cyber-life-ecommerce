aeebcdd7c345fd9b2856f75afbf0cbbfe1771670
高健翔 <g@gaojianxiangdeMacBook-Pro.local>
2024-09-06 12:28:28 +0500

    create add button initial ui

---
+++ app/Components/add-content-button.jsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function AddContentButton() {
+  return (
+    <button className="w-20 h-28 bg-transparent border-2 border-dashed border-gray-500 text-gray-600 font-semibold text-lg rounded-lg hover:bg-gray-100 hover:text-gray-800 transition-colors duration-300 flex items-center justify-center">
+      Add +
+    </button>
+  );
+}
