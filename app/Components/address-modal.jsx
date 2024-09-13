4fd96e11a9e43767d7ac379a8ec5ec5c479dc503
macjik <mcjik@icloud.com>
2024-09-13 11:31:04 +0500

    create modal ui
    added form, submission and closing options

--- app/Components/address-modal.jsx
+++ app/Components/address-modal.jsx
@@ -1,14 +1,50 @@
 'use client';
 
+import { useState } from 'react';
+import Button from './button';
 import Form from './form';
 import FormInput from './form-input';
 import Select from './select';
 
 export default function AddressModal() {
+  const cities = [
+    { label: 'Toshkent', value: 'toshkent' },
+    { label: 'Toshkent Viloyati', value: 'tashkent-viloyati' },
+    { label: 'Sirdaryo', value: 'sirdaryo' },
+    { label: 'Andijan', value: 'andijan' },
+    { label: 'Ferghana', value: 'ferghana' },
+    { label: 'Namangan', value: 'namangan' },
+    { label: 'Qashqadaryo', value: 'qashqadaryo' },
+    { label: 'Surxandaryo', value: 'surxandaryo' },
+    { label: 'Jizzax', value: 'jizzax' },
+    { label: 'Navoyi', value: 'navoyi' },
+    { label: 'Xorazm', value: 'xorazm' },
+    { label: 'Buxoro', value: 'buxoro' },
+    { label: 'Qora Qalpoqston', value: 'qora-qalpoqston' },
+  ];
+
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormSubmitted(true);
+  };
+
   return (
-    <Form title="Address">
-      <Select options={['Tashkent']}></Select>
-      <FormInput id="street" label="Street" type="text" />
-    </Form>
+    <>
+      {!formSubmitted && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="bg-white w-full h-full md:w-1/2 p-8 rounded-lg shadow-lg overflow-auto">
+            <Form onSubmit={handleSubmit} title="Your Address">
+              <Select id="select" label="Select Your City" placeholder="Select your city">
+                {cities}
+              </Select>
+              <FormInput id="street" label="Street Name" placeholder="Kal, 19" type="text" />
+              <Button type="submit">Submit</Button>
+            </Form>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
--- app/Components/select.jsx
+++ app/Components/select.jsx
@@ -1,21 +1,26 @@
 export default function Select({
   label,
-  options,
+  children,
   value,
-  onChange,
+  onChange = null,
   placeholder,
-  customClasses = '',
+  className = '',
+  id,
 }) {
   return (
-    <div className={`relative w-full ${customClasses}`}>
-      {label && <label className="block mb-2 text-sm font-medium text-gray-700">{label}</label>}
+    <div>
+      <label htmlFor={id} className=" max-w-max block text-sm font-medium text-gray-700 mb-2">
+        {label}
+      </label>
       <select
-        className="w-full p-3 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+        id={id}
+        name={id}
+        className={`${className} w-full p-3 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 text-black`}
         value={value}
         onChange={onChange}
       >
         {placeholder && <option value="">{placeholder}</option>}
-        {options.map((option, idx) => (
+        {children.map((option, idx) => (
           <option key={idx} value={option.value}>
             {option.label}
           </option>
