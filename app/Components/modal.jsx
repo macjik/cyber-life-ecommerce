052ee059a68e63699b46ea657c8e0115d48dd155
macjik <mcjik@icloud.com>
2024-09-16 10:03:37 +0500

    format

--- app/Components/modal.jsx
+++ app/Components/modal.jsx
@@ -1,6 +1,8 @@
 export default function Modal({ children, className = '' }) {
   return (
-    <div className={`${className} fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 w-max-w h-max-h`}>
+    <div
+      className={`${className} fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 w-max-w h-max-h`}
+    >
       <div className="bg-white w-full h-full md:w-1/2 p-8 rounded-lg shadow-lg overflow-auto">
         {children}
       </div>
