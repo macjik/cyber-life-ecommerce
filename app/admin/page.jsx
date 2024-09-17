0114c87117692492657a196463757c7153336651
macjik <mcjik@icloud.com>
2024-09-17 11:47:30 +0500

    table for managing content

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,5 +1,34 @@
 'use server';
 
-export default async function AdminPage() {
-    return <h1>Admin Panel</h1>
+import Dashboard from '../Components/content-dashboard';
+
+const items = [
+  {
+    id: 'adq313rdwaef-23ed',
+    image: '/image.png',
+    price: 32,
+    description: 'very good osh',
+    discount: 43,
+    isAvailable: true,
+    quantity: 3,
+    title: 'Osh',
+  },
+  {
+    id: 'adq313rdwaewf-23ed',
+    image: '/image.png',
+    price: 33,
+    description: 'very bad osh',
+    discount: 40,
+    isAvailable: true,
+    quantity: 3,
+    title: 'Posh',
+  },
+];
+
+export default async function AdminPanel() {
+  return (
+    <main className="w-full h-full">
+      <Dashboard>{items}</Dashboard>
+    </main>
+  );
 }
