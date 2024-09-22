20f137a351383f73e59c07aaabe774de2677ce85
macjik <mcjik@icloud.com>
2024-09-22 14:08:26 +0500

    format

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -2,6 +2,6 @@
 
 import EditProfile from '../Components/edit-profile';
 
-export default async function ProfilePage({searchParams}) {
-    return <EditProfile id={searchParams.id} />;
+export default async function ProfilePage({ searchParams }) {
+  return <EditProfile id={searchParams.id} />;
 }
