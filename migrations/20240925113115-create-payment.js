061a10036ab67ec8fdfafc6e83adfc95b18fbc2c
macjik <mcjik@icloud.com>
2024-09-25 16:38:08 +0500

    migrate tables for payments, orders and categories

---
+++ migrations/20240925113115-create-payment.js
@@ -0,0 +1,33 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Payments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      status: {
+        type: Sequelize.ENUM('pending', 'successful', 'failed'),
+        defaultValue: 'pending',
+        statusCode: Sequelize.INTEGER,
+        region: Sequelize.STRING,
+        cardType: Sequelize.STRING,
+        cardNumbers: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Payments');
+  },
+};
