fe21a532aef0f302c0b0b31c5b238a6718a638e2
macjik <mcjik@icloud.com>
2024-09-26 10:35:46 +0500

    invite link copy

--- app/Components/copy-button-link.jsx
+++ app/Components/copy-button-link.jsx
@@ -2,26 +2,28 @@
 
 import Button from './button';
 
-export default function CopyButtonLink({ item }) {
+export default function CopyButtonLink({ item, children = 'Copy product Link', className = '' }) {
   const productLink = `${process.env.NEXT_PUBLIC_FRONTEND_HOST}${item.toString()}`;
 
-  const handleCopy = () => {
+  function handleCopy() {
     navigator.clipboard.writeText(productLink).then(() => {
       const button = document.getElementById(`copy-button-${item.toString()}`);
       if (button) {
         button.textContent = 'Link Copied!';
         setTimeout(() => {
-          button.textContent = 'Copy Product Link';
+          button.textContent = children;
         }, 2000);
       }
     });
-  };
+  }
 
   return (
-    <div>
-      <Button className="max-w-max" id={`copy-button-${item.toString()}`} onClick={handleCopy}>
-        Copy Product Link
-      </Button>
-    </div>
+    <Button
+      className={`${className}`}
+      id={`copy-button-${item.toString()}`}
+      onClick={handleCopy}
+    >
+      {children}
+    </Button>
   );
 }
--- app/cart/[product]/page.jsx
+++ app/cart/[product]/page.jsx
@@ -3,10 +3,14 @@
 import Link from '@/node_modules/next/link';
 import Product from '../../Components/product';
 import Button from '../../Components/button';
+import CopyButtonLink from '@/app/Components/copy-button-link';
 
 export default async function CartPage({ params }) {
   const { product } = params;
+  //get user id
+  let invitee = null;
 
+  console.log('cart page params' + JSON.stringify(params));
   return (
     <Product productName={product}>
       <Link href={`/`}>
@@ -14,11 +18,9 @@ export default async function CartPage({ params }) {
           Pay
         </Button>
       </Link>
-      <Link href={``}>
-        <Button className="bg-blue-400 text-lg hover:bg-blue-500 transition duration-300 ease-in-out mt-3">
-          Share the link and get a discount
-        </Button>
-      </Link>
+      <CopyButtonLink item={`/invite?=${invitee}`} className="mt-2">
+        Share the link and buy it cheaper
+      </CopyButtonLink>
     </Product>
   );
 }
