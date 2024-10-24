e9a92fe475f5da6d9dac09f534ef765acd4b4beb
macjik <mcjik@icloud.com>
2024-10-24 11:06:21 +0500

    loading logo for pages with suspense

--- app/Components/loading.jsx
+++ app/Components/loading.jsx
@@ -2,8 +2,13 @@ import Logo from './logo';
 
 export default function Loading() {
   return (
-    <div className="w-full h-full flex justify-center">
-      <Logo />
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="animate-pulse">
+        <div className="h-32 w-32 sm:h-28 sm:w-28 md:h-36 md:w-36 lg:h-48 lg:w-48">
+          <Logo />
+          <p className="text-center">Loading...</p>
+        </div>
+      </div>
     </div>
   );
 }
