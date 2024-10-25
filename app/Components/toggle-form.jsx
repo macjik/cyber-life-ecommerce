6df7f7806e8d3b5ff222c79a5164266b02c0485b
macjik <mcjik@icloud.com>
2024-10-25 09:19:00 +0500

    Revert "button colour and style changes"
    This reverts commit 4d39aa8c9e785907e9a5e840e663d96e2cfbc856.

--- app/Components/toggle-form.jsx
+++ app/Components/toggle-form.jsx
@@ -16,21 +16,11 @@ export default function AuthForm() {
     <>
       {!isSignup ? (
         <PreLoginForm>
-          <Button
-            className="bg-white border-blue-600 border-2 rounded-l-none rounded-r text-blue-600 hover:bg-blue-600 hover:text-white"
-            onClick={toggleSwitch}
-          >
-            Create account
-          </Button>
+          <Button onClick={toggleSwitch}>I do not have an account</Button>
         </PreLoginForm>
       ) : (
         <PreSigninForm>
-          <Button
-            className="bg-white border-blue-600 border-2 rounded-l-none rounded-r text-blue-600 hover:bg-blue-600 hover:text-white"
-            onClick={toggleSwitch}
-          >
-            I have an account
-          </Button>
+          <Button onClick={toggleSwitch}>I already have an account</Button>
         </PreSigninForm>
       )}
     </>
