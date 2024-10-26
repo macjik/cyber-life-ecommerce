1dcb081824883ad26703b8369529f15987e33721
macjik <mcjik@icloud.com>
2024-10-26 21:36:32 +0500

    navbar translation

--- app/Components/navbar.jsx
+++ app/Components/navbar.jsx
@@ -3,10 +3,12 @@
 import Link from 'next/link';
 import { FaUser, FaShoppingCart, FaHome } from 'react-icons/fa';
 import { AiFillControl } from 'react-icons/ai';
+import { getTranslations } from 'next-intl/server';
 
 export default async function NavBar({ userRole, isAllowedRoute, children, icon }) {
   if (!isAllowedRoute) return null;
 
+  const t = await getTranslations();
   return (
     <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 flex justify-around items-center h-16 z-50">
       <Link
@@ -14,21 +16,21 @@ export default async function NavBar({ userRole, isAllowedRoute, children, icon 
         className="flex flex-col items-center text-gray-600 hover:text-blue-500 w-full"
       >
         <FaUser size={24} />
-        <span className="text-xs">User</span>
+        <span className="text-xs">{t('user')}</span>
       </Link>
       <Link
         href="/"
         className="flex flex-col items-center text-gray-600 hover:text-blue-500 w-full"
       >
         <FaHome size={24} />
-        <span className="text-xs">Home</span>
+        <span className="text-xs">{t('home')}</span>
       </Link>
       <Link
         href="/my-cart"
         className="flex flex-col items-center text-gray-600 hover:text-blue-500 w-full"
       >
         <FaShoppingCart size={24} />
-        <span className="text-xs">Cart</span>
+        <span className="text-xs">{t('cart')}</span>
       </Link>
       {userRole === 'admin' && (
         <Link
