4061301e582799a1bfbf3a149cb80d2ec179ae84
macjik <mcjik@icloud.com>
2024-10-28 12:01:52 +0500

    optionState status false button styling fix

--- app/Components/product-options.jsx
+++ app/Components/product-options.jsx
@@ -26,7 +26,7 @@ export default function ProductOptions({ orderId, children }) {
       id={orderId}
       className={`inline-block px-4 py-2 rounded-full ms-2 border-2 border-orange-400 
         ${optionState?.status === 200 ? 'bg-orange-400 text-white' : ''}
-        ${optionState?.status === false ? 'bg-orange-400 text-white' : ''}
+        ${optionState?.status === false ? 'text-orange-400' : ''}
         ${optionState?.status !== 200 && optionState?.status !== false ? 'text-orange-400 border-2 border-orange-400' : ''}
         hover:bg-orange-400 hover:text-white`}
     >
