44db6db8ff523002e614434e17c1ffe67699f60e
macjik <mcjik@icloud.com>
2024-10-29 16:53:05 +0500

    credit card logos in footer

--- app/Components/footer.jsx
+++ app/Components/footer.jsx
@@ -2,7 +2,7 @@
 
 import Image from '@/node_modules/next/image';
 import Link from '@/node_modules/next/link';
-import {getTranslations} from 'next-intl/server';
+import { getTranslations } from 'next-intl/server';
 
 export default async function Footer() {
   const t = await getTranslations('footer');
@@ -21,9 +21,7 @@ export default async function Footer() {
         </div>
         <section className="mb-4 md:mb-0 max-w-max h-max text-sm">
           <h2 className="text-xl font-semibold mb-2 max-w-max">{t('about-title')}</h2>
-          <p className="font-medium text-lg">
-            {t('about-text')}
-          </p>
+          <p className="font-medium text-lg">{t('about-text')}</p>
         </section>
 
         <section className="mb-4 md:mb-0 w-max h-max text-sm">
@@ -53,9 +51,41 @@ export default async function Footer() {
         </section>
         <section className="mb-10 md:mb-0 text-sm max-w-max h-max space-y-2">
           <h2 className="text-xl font-semibold mb-2">{t('payment-methods-title')}</h2>
-          <p className="text-lg font-medium">
-           {t('payment-methods-text')}
-          </p>
+          <p className="text-lg font-medium">{t('payment-methods-text')}</p>
+          <div className="w-full inline-flex justify-center gap-4">
+            <Image
+              src="/humo_card_transparent.png"
+              alt="mimi logo"
+              quality={60}
+              width={60}
+              height={100}
+              className="object-contain"
+            />
+            <Image
+              src="/uzcard_transparent.png"
+              alt="mimi logo"
+              quality={60}
+              width={60}
+              height={100}
+              className="object-contain"
+            />
+            <Image
+              src="/visa.png"
+              alt="mimi logo"
+              quality={60}
+              width={60}
+              height={100}
+              className="object-contain"
+            />
+            <Image
+              src="/mastercard.png"
+              alt="mimi logo"
+              quality={60}
+              width={60}
+              height={100}
+              className="object-contain"
+            />
+          </div>
         </section>
         <section className="mb-10 md:mb-0 text-sm max-w-max h-max space-y-2">
           <h2 className="text-xl font-semibold mb-2">{t('contacts')}</h2>
