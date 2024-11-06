85f520a1c9d13bd5033981c584ad96722410ca55
macjik <mcjik@icloud.com>
2024-11-06 10:32:59 +0500

    import useEffect

--- app/Components/generate-invite.jsx
+++ app/Components/generate-invite.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import axios from 'axios';
 import axiosRetry from 'axios-retry';
 import Button from './button';
