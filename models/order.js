c37b074a14b1f931eaa71b2d8ff243ddeb9b2ade
macjik <mcjik@icloud.com>
2024-11-08 15:41:20 +0500

    remove relationships

--- models/order.js
+++ models/order.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
       Order.belongsTo(User, { foreignKey: 'userId' });
       Order.belongsTo(item, { foreignKey: 'itemId' });
       Order.belongsTo(Item_Attribute, { foreignKey: 'item_attribute_id' });
-      Order.belongsToMany(Invite, { through: 'OrderInvites', foreignKey: 'orderId' });
+      // Order.belongsToMany(Invite, { through: 'OrderInvites', foreignKey: 'orderId' });
       Order.hasOne(Payment, { foreignKey: 'orderId', as: 'payment' });
     }
   }
