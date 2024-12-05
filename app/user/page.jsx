5b082c05c5c66333a8e1addea36fa42b16b990ec
macjik <mcjik@icloud.com>
2024-12-05 12:27:20 +0500

    check if user has already applied to company registration form

--- app/user/page.jsx
+++ app/user/page.jsx
@@ -5,6 +5,7 @@ import UserProfile from '../Components/user-profile';
 import SubNav from '../Components/sub-nav';
 import { FaShoppingCart, FaCommentDots, FaHandshake, FaLink, FaLanguage } from 'react-icons/fa';
 import { getTranslations } from 'next-intl/server';
+import client from '../services/redis';
 
 const User = db.User;
 
@@ -15,6 +16,7 @@ export default async function UserPage({ searchParams }) {
   const { name, phone, address, sub, id, image, role } = user;
 
   const t = await getTranslations('profile');
+  let hasApplied = await client.get(userId);
   return (
     <main className="w-full min-h-screen flex-row justify-center items-center overflow-hidden">
       <UserProfile name={name} userId={id} phone={phone} address={address} image={image} />
@@ -24,7 +26,7 @@ export default async function UserPage({ searchParams }) {
       {/* <SubNav faIcon={<FaCommentDots size={24} />} link="/feedback">
         My feedback
         </SubNav> */}
-      {role !== 'owner' ? (
+      {role !== 'owner' && !hasApplied ? (
         <SubNav faIcon={<FaHandshake size={24} />} link="/shop">
           {t('partnership')}
         </SubNav>
