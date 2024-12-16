8e71624c2bee62efc0efcab50277c8c7e57ace87
macjik <mcjik@icloud.com>
2024-12-16 14:36:45 +0500

    life modal fix

--- app/Components/life-modal.jsx
+++ app/Components/life-modal.jsx
@@ -43,45 +43,42 @@ export default function LifeModal({ placeholder = '' }) {
 
   console.log(invoiceReqState);
   console.log(placeholder);
+
   return (
-    <Form title={service} action={invoiceReqAction} className="w-full gap-3">
-      {/* <div className="w-full inline-flex mt-4">
-        {placeholder === 'Phone Number' && (
-          <div className="bg-slate-300 text-gray-600 border-2 rounded-l border-gray-300 h-9 font-medium text-center p-1 text-sm">
-            +998
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+      <div className="p-6 rounded-lg w-full max-w-lg relative">
+        <Form title={service} action={invoiceReqAction} className="w-full gap-3">
+          <FormInput
+            className="w-full"
+            inputMode="tel"
+            id="target"
+            placeholder={placeholder}
+            type="tel"
+          />
+          <FormInput
+            className="w-full"
+            inputMode="tel"
+            id="amount"
+            placeholder="1000-999 000 UZS"
+            type="number"
+            maxLength="6"
+            minLength="3"
+            label="Amount"
+          />
+          <input type="hidden" value={id} name="id" />
+          <input type="hidden" value={service} name="service" />
+          <div className="flex justify-end space-x-4">
+            <SubmitButton>Confirm</SubmitButton>
+            <button
+              type="button"
+              onClick={handleCancel}
+              className="px-4 py-2 bg-gray-300 text-gray-700 rounded-md shadow-md hover:bg-gray-400 focus:outline-none"
+            >
+              Cancel
+            </button>
           </div>
-        )} */}
-        <FormInput
-          className='w-full'
-          inputMode="tel"
-          id="target"
-          placeholder={placeholder}
-          type="tel"
-          // maxLength="9"
-        />
-      {/* </div> */}
-      <FormInput
-        className="w-full"
-        inputMode="tel"
-        id="amount"
-        placeholder="1000-999 000 UZS"
-        type="number"
-        maxLength="6"
-        minLength="3"
-        label="Amount"
-      />
-      <input type="hidden" value={id} name="id" />
-      <input type="hidden" value={service} name="service" />
-      <div className="flex justify-end space-x-4">
-        <SubmitButton>Confirm</SubmitButton>
-        <button
-          type="button"
-          onClick={handleCancel}
-          className="px-4 py-2 bg-gray-300 text-gray-700 rounded-md shadow-md hover:bg-gray-400 focus:outline-none"
-        >
-          Cancel
-        </button>
+        </Form>
       </div>
-    </Form>
+    </div>
   );
 }
