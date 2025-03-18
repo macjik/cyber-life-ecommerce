14417e6ca38759f93ec18c3ff592da4a25b3b3cc
macjik <mcjik@icloud.com>
2025-03-18 17:32:45 +0500

    Revert "spacing"
    This reverts commit ea17ae9c940349360a9a3d40a6f56d338f120de7.

--- app/page.jsx
+++ app/page.jsx
@@ -55,9 +55,7 @@ export default async function Home({ searchParams }) {
             <h1 className="text-5xl font-extrabold text-gray-900">
               {company.name || <>Mimi: {t('title')}</>}
             </h1>
-            <p className="text-lg text-gray-700 sm:text-sm">
-              {company.slogan || <>{t('slogan')}</>}
-            </p>
+            <p className="text-lg text-gray-700">{company.slogan || <>{t('slogan')}</>}</p>
             {/* <div className="mb-4 md:mb-0 w-full h-full flex justify-center items-center mt-10 md:mt-10"> */}
             {/* <img
                 src="/transparent-mimi-logo.png"
@@ -128,7 +126,7 @@ export default async function Home({ searchParams }) {
           </section>
           <section className="w-full max-w-4xl h-max-h flex flex-col justify-center items-center text-center space-y-4">
             <h2 className="text-3xl font-semibold text-gray-800">{t('instruction-header')}</h2>
-            <ol className="no-markers text-left ms-24 sm:ms-0 list-decimal space-y-2 text-lg text-gray-700 sm:text-sm">
+            <ol className="no-markers text-left ms-24 list-decimal space-y-2 text-lg text-gray-700">
               <li>{t('instruction-1')}</li>
               <li>{t('instruction-2')}</li>
               <li>{t('instruction-3')}</li>
@@ -170,7 +168,7 @@ export default async function Home({ searchParams }) {
       <main className="w-full flex flex-col items-center pt-4 pb-20 space-y-6 min-h-screen mt-20">
         <section className="w-full max-w-4xl h-1/3 flex flex-col justify-center items-center text-center space-y-4 px-10">
           <h1 className="text-5xl font-extrabold text-gray-900">Mimi: {t('title')}</h1>
-          <p className="text-lg text-gray-700 sm:text-sm">{t('slogan')}</p>
+          <p className="text-lg text-gray-700">{t('slogan')}</p>
           {/* <div className="mb-4 md:mb-0 w-full h-full flex justify-center items-center mt-10 md:mt-10"> */}
           {/* <img
               src="/transparent-mimi-logo.png"
@@ -224,9 +222,9 @@ export default async function Home({ searchParams }) {
             )}
           </div>
         </section>
-        <section className="w-full max-w-4xl h-max-h flex flex-col justify-center items-center text-center space-y-4 px-10">
+        <section className="w-full max-w-4xl h-max-h flex flex-col justify-center items-center text-center space-y-4">
           <h2 className="text-3xl font-semibold text-gray-800">{t('instruction-header')}</h2>
-          <ol className="no-markers text-left list-disc space-y-2 text-lg text-gray-700 sm:text-sm sm:ms-0">
+          <ol className="no-markers text-left ms-24 list-decimal space-y-2 text-lg text-gray-700">
             <li>{t('instruction-1')}</li>
             <li>{t('instruction-2')}</li>
             <li>{t('instruction-3')}</li>
